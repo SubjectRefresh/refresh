f09927244e169c6dd2bfcdc05137372a84239a88
Pombearman <miles@budden.net>
2015-07-27 13:33:44 +0100

    Initial code

--- convert.js
+++ convert.js
@@ -1,1 +1,10 @@
-Lorem Ipsum
+var myStringArray = ["Hello", "World"];
+
+function convert(inputArray) {
+    var arrayLength = inputArray.length;
+    for (var i = 0; i < arrayLength; i++) {
+        console.log(inputArray[i]);
+}
+}
+
+convert(myStringArray);
