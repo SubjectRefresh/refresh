2dc48d9470a4dc277eb64590591903d6c0a591b2
Pombearman <miles@budden.net>
2015-07-27 15:16:21 +0100

    Setup for sentence analysis

--- convert.js
+++ convert.js
@@ -1,12 +1,15 @@
 var myStringArray = ["State the distinguishing properties of solids, liquids and gases", "Describe the structure of solids, liquids and gases in terms of particle separation, arrangement and types of motion", "Describe changes of state in terms of melting, boiling, evaporation, freezing, condensation and sublimation", "Describe qualitatively the pressure and temperature of a gas in terms of the motion of its particles", "Show an understanding of the random motion of particles in a suspension (sometimes known as Brownian motion) as evidence for the kinetic particle (atoms, molecules or ions) model of matter", "Describe and explain diffusion"];
 
-var convertModule = function() {
+var convertModule = function () {
     var self = this;
-    
-    self.covert = function(inputArray, callback) {
+
+    self.covert = function (inputArray, callback) {
         var roots = [];
         var arrayLength = inputArray.length;
         for (var i = 0; i < arrayLength; i++) {
+            //=====================================================================================
+            //Holding sentence
+
             //console.log("Current working sentence: " + inputArray[i]);
             var sentence = inputArray[i]
             for (c = 0; c < sentence.length; c++) {
@@ -18,6 +21,8 @@ var convertModule = function() {
             }
             //console.log(root);
             roots.push(root);
+
+            //====================================================================================            
         }
         callback(roots);
     };
