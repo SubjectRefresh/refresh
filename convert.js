d7b79f4e915ac8a69fbb25d32a130ee79c6ae40b
Oli Callaghan <OliCallaghan@icloud.com>
2015-07-27 20:53:18 +0100

    Exported the functions in convert.js

--- convert.js
+++ convert.js
@@ -1,5 +1,5 @@
 var myStringArray = ["State the distinguishing properties of solids, liquids and gases", "State which parts of YRS are the best", "Describe the structure of solids, liquids and gases in terms of particle separation, arrangement and types of motion", "Describe changes of state in terms of melting, boiling, evaporation, freezing, condensation and sublimation", "Describe qualitatively the pressure and temperature of a gas in terms of the motion of its particles", "Show an understanding of the random motion of particles in a suspension (sometimes known as Brownian motion) as evidence for the kinetic particle (atoms, molecules or ions) model of matter", "Describe and explain diffusion"];
-//
+
 var convertModule = function () {
     var self = this;
 
@@ -53,58 +53,55 @@ var convertModule = function () {
         callback(output);
 
     };
+    
+    self.convertActual = function(inputArray, callback) {
+        var root = "";
+        var question = "";
+        var outputContentWrapper = [];
+        var output = []
 
-};
+        var arrayLength = inputArray.length;
 
-module.exports = convertModule;
+        for (var i = 0; i < arrayLength; i++) {
 
-function convertActual(inputArray, callback) {
+            var sentence = inputArray[i]
 
-    var root = "";
-    var question = "";
-    var outputContentWrapper = [];
-    var output = []
+            for (c = 0; c < sentence.length; c++) {
 
-    var arrayLength = inputArray.length;
+                if (sentence[c] == " ") {
 
-    for (var i = 0; i < arrayLength; i++) {
+                    var root = sentence.slice(c + 1, sentence.length)
+                    var keyword = sentence.slice(0, c);
+                    break;
 
-        var sentence = inputArray[i]
+                }
 
-        for (c = 0; c < sentence.length; c++) {
+            }
+            console.log("Keyword:--" + keyword + "--");
 
-            if (sentence[c] == " ") {
+            switch (keyword) {
 
-                var root = sentence.slice(c + 1, sentence.length)
-                var keyword = sentence.slice(0, c);
-                break;
+                case "State":
+
+                    console.log("Switch for State");
+                    var words = sentence.split(" ");
+                    words.shift();
+                    root = words;
+                    words.unshift("What", "are");
+                    words.push("?");
+                    console.log("QUESTION: " + words.join(" "));
+                    question = words.join(" ")
+                    outputContentWrapper.push(root);
+                    outputContentWrapper.push(question);
+                    output.push(outputContentWrapper);
+                    break;
 
             }
 
         }
-        console.log("Keyword:--" + keyword + "--");
 
-        switch (keyword) {
+        callback(output);
+    };
+};
 
-            case "State":
-
-                console.log("Switch for State");
-                var words = sentence.split(" ");
-                words.shift();
-                root = words;
-                words.unshift("What", "are");
-                words.push("?");
-                console.log("QUESTION: " + words.join(" "));
-                question = words.join(" ")
-                outputContentWrapper.push(root);
-                outputContentWrapper.push(question);
-                output.push(outputContentWrapper);
-                break;
-
-        }
-
-    }
-
-    callback(output);
-}
-convertActual(myStringArray, function (output) {})
+module.exports = convertModule;
