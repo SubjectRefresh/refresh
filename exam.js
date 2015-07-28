031ee64581c4831eaed91bd87a7f9b8c460a5d95
Popey Gilbert <popey@debenclipper.com>
2015-07-28 14:37:48 +0100

    Logging Console + Colours

--- exam.js
+++ exam.js
@@ -1,7 +1,9 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-var color = require('color');
+var colors = require('colors');
+
+console.log("Exam.JS:".bold + " Successfully Imported Required Packages".green);
 
 var examBoardModule = function() {
     var self = this;
@@ -9,6 +11,7 @@ var examBoardModule = function() {
     self.examBoardCIE = function(callback) {
         request('http://www.cie.org.uk/programmes-and-qualifications/cambridge-secondary-2/cambridge-igcse/subjects/', function(error, response, body) {
             if (!error && response.statusCode == 200) {
+                console.log("Exam.JS:".bold + " Successfully Requested Subject List".green);
                 $ = cheerio.load(body);
 
                 data = cleanArray($('.emphasized-link').text().replace(/ +?|\r/g, '').split("\n"));
@@ -25,7 +28,8 @@ var examBoardModule = function() {
                 }
 
                 subjectArray.length = subjectArray.length - 4;
-                console.log(subjectArray);
+                //console.log(subjectArray);
+                console.log("Exam.JS:".bold + " Successfully Got `subjectArray`".green);
                 //callback(subjectArray); //Future Callback
             }
         });
@@ -41,6 +45,7 @@ var examBoardModule = function() {
         return newArray;
     }
     //self.examBoardCIE(); //Temporary Use for Testing
+    console.log("Exam.JS:".bold + " Successfully Defined `examBoardCIE`".green);
 };
 
 module.exports = examBoardModule;
