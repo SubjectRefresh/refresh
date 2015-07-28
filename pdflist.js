f5f2f9c31f3e80d9f4989c7a0d5696f4a765340a
Finnian Anderson <finnian@fxapi.co.uk>
2015-07-28 23:52:56 +0100

    Attempt to get correct listings of PDF
    Introduces a bug where the callback from `collectURL()` in
    `test_pdf.js` is being called before `collectURLs()` has finished,
    leaving the array passed to the callback unchanged - line `62`.

--- pdflist.js
+++ pdflist.js
@@ -1,6 +1,6 @@
 var request = require("request");
 var cheerio = require("cheerio");
-var colors = require ("colors");
+var colors = require("colors");
 console.log("PDFList.js: ".bold + " Successfully Imported Required Packages".green);
 
 
@@ -9,49 +9,56 @@ Array.prototype.last = function() {
 }
 console.log("PDFList.js: ".bold + " Successfully Declared `last`".green);
 
+var collectModule = function() {
+    var self = this;
 
-function collectURL(number, callback) { // returns an array of the links and numbers for each syllabus
-  // PLEASE PASS <number> AS A STRING!
-    var link = [];
-    var newArray = [];
-    console.log("PDFList.js: ".bold + " Successfully Defined Global Variables".green);
-    request("http://www.cie.org.uk/programmes-and-qualifications/cambridge-secondary-2/cambridge-igcse/subjects/", function(error, response, body) {
-        if (!error && response.statusCode == 200){
-              console.log("PDFList.js: ".bold + " Successfully Requested Website".green);
-              $ = cheerio.load(body);
-              $(".emphasized-link").find("li").each(function() { // loop through each link
-                  var selector = $(this).find("a").attr("href");
-                  newArray.push({
-                    "dom_object": $(this),
-                    "number": selector.split("-").last().replace("/",""), // number
-                    "link": selector // link
+    self.collectURLs = function(number, callback) { // returns an array of the links and numbers for each syllabus
+        // PLEASE PASS <number> AS A STRING!
+        var newArray = [];
+        var links = {
+            syllabus: null,
+            pdfs: []
+        };
+        console.log("PDFList.js: ".bold + "Successfully Defined Global Variables".green);
+        request("http://www.cie.org.uk/programmes-and-qualifications/cambridge-secondary-2/cambridge-igcse/subjects/", function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("PDFList.js: ".bold + "Successfully Requested Website For List of Subjects".green);
+                $ = cheerio.load(body);
+                $(".emphasized-link").find("li").each(function() { // loop through each link
+                    var selector = $(this).find("a").attr("href");
+                    newArray.push({
+                        "dom_object": $(this), // this is so we can access it later
+                        "number": selector.split("-").last().replace("/", ""), // number
+                        "link": baseURL + selector // link
                     });
-              });
-              console.log("PDFList.js: ".bold + " Successfully Pushed to `newArray`".green);
-              for (i=0;i<newArray.length;i++){
-                if (String(newArray[i].number) == String(number)){ // we got a match for the subject
-                  link = newArray[i].link;
-                  request("http://www.cie.org.uk/" + newArray[i].dom_object.find("a").attr("href"), function(error2, response2, body2){
-                    $new = cheerio.load(body2);
-                    $new(".binaryLink").find("a").each(function(){
-                      var PDFLink = $new(this).attr("href");
-                      console.log("PDFList.js: ".bold + $new(this).text().red + " has a link of " + PDFLink.red);
-                    });
-                 });
+                });
+                for (i = 0; i < newArray.length; i++) {
+                    if (String(newArray[i].number) == String(number)) { // we got a match for the subject
+                        links.syllabus = newArray[i].link;
+                        request(baseURL + newArray[i].dom_object.find("a").attr("href"), function(error2, response2, body2) {
+                            console.log("PDFList.js: ".bold + "Successfully Requested Website For List Of PDFs".green);
+                            $new = cheerio.load(body2);
+                            $new(".binaryLink").find("a").each(function() {
+                                var PDFLink = $new(this).attr("href");
+                                links.pdfs.push(PDFLink); // is working
+                                console.log("PDFList.js: ".bold + $new(this).text().blue + " => " + baseURL.green + PDFLink.green);
+                            });
+                            console.log("PDFList.js: ".bold + "Got ".green + String(links.pdfs.length).blue + " PDFs".green);
+                        });
+                    }
                 }
-              }
-          newArray.splice(newArray.length-7, 7);
-        }
-        console.log("PDFList.js: ".bold + " Successfully Completed `collectURL`".green);
-        if (callback !== undefined) {
-          callback(link);
-        }
-    });
+            }
+            if (callback !== undefined) {
+                callback(links);
+            }
+        });
+    }
 }
-
 var number = "0600";
-collectURL(number,function(link){
-  console.log("PDFList.js: ".bold + "The link for " + number.red + " is " + link.red);
+collectURLs(number, function(link) {
+    console.log("PDFList.js: ".bold + "The link for " + number.red + " is " + link.red);
 });
 
 console.log("PDFList.js: ".bold + " Successfully Declared `collectURL`".green);
+
+module.exports = collectModule;
