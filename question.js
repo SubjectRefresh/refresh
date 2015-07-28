51ed6f560cb891e2e798a9351a55baef3d224bc2
Pombearman <miles@budden.net>
2015-07-28 14:17:50 +0100

    Fixes

--- question.js
+++ question.js
@@ -1,11 +1,12 @@
 var myStringArray = ["State the distinguishing properties of solids, liquids and gases", "State which parts of YRS are the best", "Describe the structure of solids, liquids and gases in terms of particle separation, arrangement and types of motion", "Describe changes of state in terms of melting, boiling, evaporation, freezing, condensation and sublimation", "Describe qualitatively the pressure and temperature of a gas in terms of the motion of its particles", "Show an understanding of the random motion of particles in a suspension (sometimes known as Brownian motion) as evidence for the kinetic particle (atoms, molecules or ions) model of matter", "Describe and explain diffusion"];
 
 var utf8 = require('utf8');
+var request = require("request");
 
 var questionModule = function () {
     var self = this;
 
-    self.covert = function (inputArray, callback) {
+    self.convert = function (inputArray, callback) {
 
         var root = "";
         var question = "";
@@ -29,15 +30,9 @@ var questionModule = function () {
                 }
 
             }
-            request("http://api.textrazor.com/?text=" + utf8.encode(sentence), function (err, res, body) {
-                console.log(body);
-                callback(output);
-            });
         }
 
-    }
-
-
+    };
 
 };
 
