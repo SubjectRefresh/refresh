36d5489ab3dd8b4982b82806f2cb1a674a89ab9a
FInnian Anderson <finnian@fxapi.co.uk>
2015-07-28 14:16:45 +0100

    Revert fd305db..76b59ef
    This rolls back to commit fd305db7d2a83e4a80526c13b2e8a819910a852e.

--- pdflist.js
+++ pdflist.js
@@ -1,6 +1,10 @@
 var request = require("request");
 var cheerio = require("cheerio");
 
+function log(string){
+  console.log(string);
+}
+
 Array.prototype.last = function() {
     return this[this.length - 1];
 }
@@ -10,7 +14,7 @@ function collectURL(number, callback) { // returns an array of the links and num
     var link = [];
     var newArray = [];
     request('http://www.cie.org.uk/programmes-and-qualifications/cambridge-secondary-2/cambridge-igcse/subjects/', function(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode == 200){
               $ = cheerio.load(body);
               $(".emphasized-link").find("li").find("a").each(function() { // loop through each link
                   var selector = $(this).attr("href");
@@ -26,6 +30,7 @@ function collectURL(number, callback) { // returns an array of the links and num
               }
           newArray.splice(newArray.length-7, 7);
         }
+        log(link);
         if (callback !== undefined) {
           callback(link);
         }
