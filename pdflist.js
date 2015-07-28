2e997ba84fe4b5f47eb05bf0b376cd2329a315fb
FInnian Anderson <finnian@fxapi.co.uk>
2015-07-28 14:12:26 +0100

    Remove necessary console.log()

--- pdflist.js
+++ pdflist.js
@@ -14,7 +14,7 @@ function collectURL(number, callback) { // returns an array of the links and num
     var link = [];
     var newArray = [];
     request('http://www.cie.org.uk/programmes-and-qualifications/cambridge-secondary-2/cambridge-igcse/subjects/', function(error, response, body) {
-        if (!error && response.statusCode == 200){
+        if (!error && response.statusCode == 200) {
               $ = cheerio.load(body);
               $(".emphasized-link").find("li").find("a").each(function() { // loop through each link
                   var selector = $(this).attr("href");
@@ -30,7 +30,6 @@ function collectURL(number, callback) { // returns an array of the links and num
               }
           newArray.splice(newArray.length-7, 7);
         }
-        log(link);
         if (callback !== undefined) {
           callback(link);
         }
