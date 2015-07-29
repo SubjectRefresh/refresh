a4c8d91ce5c3c53eb37aee8d3202f694f4e08461
Tom Emmerson <thepcbuilder1@Gmail.com>
2015-07-29 13:18:47 +0100

    Lines

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var crypto = require('crypto');
 var connection = mysql.createConnection({
     host: '185.38.45.194',
     user: 'hexcompu_ref',
@@ -6,11 +7,41 @@ var connection = mysql.createConnection({
     database: 'hexcompu_refresh'
 });
 connection.connect();
-console.log('successfull logon')
-function AddUser(fName,lName,eMail,pass){
-    connection.query('INSERT INTO UserData SET FirstName=?, LastName=?, Email=?, Password=?', [fName,lName,eMail,pass], function(err,rows,fields){
+console.log('Server Login')
+function AddUser(fName,lName,eMail,pass,uName){
+    var salt = crypto.randomBytes(128).toString('base64');
+  crypto.pbkdf2(pass, salt, 10000, 512, function(err, derivedKey) {
+    pass = derivedKey;
+   
+        
+    connection.query('INSERT INTO UserData SET FirstName=?, LastName=?, Email=?, Hash=?, UserName=?, Salt=?', [fName,lName,eMail,pass,uName,salt], function(err,rows,fields){
         if(err) throw err;
         
     });
+    });
 }
 
+function login(eMail,pass){
+    connection.query('SELECT Salt,Hash FROM UserData WHERE Email=?', [eMail], function(err,rows,fields){
+        if(err) throw err;
+        console.log(rows);
+        var salt=rows[0]['Salt'];
+        var hash = rows[0]['Hash'];
+        var both = pass+salt;
+        console.log(rows[0]['Salt'])
+        console.log(both)
+        console.log(hash+salt)
+        if(both==hash+salt){
+            console.log('SUCCESSFUL LOGIN')
+        }
+        connection.query('SELECT UID FROM UserData WHERE Email=? and Hash=? ', [eMail,pass], function(err,rows,fields){
+        if(err) throw err;
+        
+    });
+    });
+   
+    
+
+    
+}
+login('jim','dave')
