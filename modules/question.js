987e83857405186b9ba83aa9b2a45073c6137953
Pombearman <miles@budden.net>
2015-07-29 18:59:20 +0100

    Stripped question.js and made new test module question_test.js

--- modules/question.js
+++ modules/question.js
@@ -1,76 +1,25 @@
-var utf8 = require('utf8');
-var request = require("request");
 var colors = require("colors");
+var utf8 = require('utf8');
+var request = require('sync-request');
 console.log("Question.JS:".bold + " Successfully Imported Required Packages".blue);
 
 var questionModule = function () {
     var self = this;
 
-    self.question = function (inputArray, callback) {
-
-        var root = "";
-        var question = "";
-        var outputContentWrapper = [];
-        var output = []
-
-        var arrayLength = inputArray.length;
-        console.log("Question got to loading the array");
-
-        for (var i = 0; i < arrayLength; i++) {
-
-            var sentence = inputArray[i]
+    self.covert = function (inputString, callback) {
+        var output = [];
 
 
-            for (c = 0; c < sentence.length; c++) {
+        var res = request('POST', 'https://api.textrazor.com', {
+            body: "apiKey=c0dbc052930dce78cc1dd1b37b3d3a4fb3f609c251c4f7e34a3b452a&text=" + utf8.encode(inputString) + "&extractors=" + utf8.encode("entities")
+        });
+        var data = JSON.parse(res.getBody().toString('utf8'));
 
-                if (sentence[c] == " ") {
-
-                    var root = sentence.slice(c + 1, sentence.length)
-                    var keyword = sentence.slice(0, c);
-                    break;
-
-                }
-
-            }
-            var options = {
-                url: 'https://api.textrazor.com/',
-                'method': 'POST',
-                'body': {
-                    "apiKey": "c0dbc052930dce78cc1dd1b37b3d3a4fb3f609c251c4f7e34a3b452a",
-                    "text": utf8.encode(sentence),
-                    "extractors": "words"
-                }
-
-            };
-
-            request(options, function (error, response, body) {
-                console.log("TextRazer reply :: " + response.getBody());
-            });
-
-            /* developius added this */
-            console.log("fetching...");
-            request.post('http://service.com/upload', {form: {
-                apiKey:'c0dbc052930dce78cc1dd1b37b3d3a4fb3f609c251c4f7e34a3b452a',
-                text: utf8.encode(sentence),
-                extractors: "words"}
-            }, function (err, httpResponse, body){
-                if (httpResponse == 200 && !err){
-                    console.log(body);
-                }
-                else {
-                    throw err;
-                }
-            });
-            /* end of developius additions */
-        }
-
-    };
-
+        console.log(data);
+        callback(output);
+    }
+}
+console.log("Convert.JS:".bold + " Successfully Defined `convert`".blue);
 };
 
-/* developius added this */
-//questionModule.question(["random question"],function(){var x = null;}); // I've got no clue how to test the code I added above - help!
-/* end of developius additions */
-
-console.log("Question.JS:".bold + " Successfully finished question".blue);
 module.exports = questionModule;
--- question_test.js
+++ question_test.js
@@ -1,25 +1,20 @@
-var colors = require("colors");
 var utf8 = require('utf8');
 var request = require('sync-request');
-console.log("Question.JS:".bold + " Successfully Imported Required Packages".blue);
+var colors = require("colors");
 
-var questionModule = function () {
-    var self = this;
+function question(inputString, callback) {
+    var output = "";
+    var res = request('POST', 'https://api.textrazor.com', {
+        var entityPositions = [];
+        var entityPositionsContentWrapper = [];
+        body: "apiKey=c0dbc052930dce78cc1dd1b37b3d3a4fb3f609c251c4f7e34a3b452a&text=" + utf8.encode(inputString) + "&extractors=" + utf8.encode("entities")
+    });
+    var data = JSON.parse(res.getBody().toString('utf8'));
 
-    self.covert = function (inputString, callback) {
-        var output = [];
-
-
-        var res = request('POST', 'https://api.textrazor.com', {
-            body: "apiKey=c0dbc052930dce78cc1dd1b37b3d3a4fb3f609c251c4f7e34a3b452a&text=" + utf8.encode(sentence) + "&extractors=" + utf8.encode("entities")
-        });
-        var data = JSON.parse(res.getBody().toString('utf8'));
-
-
-        callback(output);
-    }
+    console.log(res.getBody().toString('utf8'));
+    callback(output);
 }
-console.log("Convert.JS:".bold + " Successfully Defined `convert`".blue);
-};
 
-module.exports = questionModule;
+question("Atoms are very small objects", function (output) {
+    console.log(output);
+});
