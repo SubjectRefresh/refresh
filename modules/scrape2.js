e1881bb428204736336da1e839806528db2a53cf
Popey Gilbert <popey@debenclipper.com>
2015-07-29 15:47:23 +0100

    Update Scrape2

--- modules/scrape2.js
+++ modules/scrape2.js
@@ -5,19 +5,24 @@ var natural = require("natural");
 
 parseHTML = function(number) {
     fs.readFile('../temporary/CIE' + number + ".html", 'utf8', function(err, data) {
-        if (err) {
-            return console.log(err);
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Success!");
+            $ = cheerio.load(data);
+            var blarg = $("body").text();
+            // console.log(blarg);
+
+            blarg = blarg.split("•");
+            console.log(blarg);
+
+            for (i = 0; i < blarg.length; i++) {
+                if blarg[i].indexOf("State the distinguishing properties of solids") > -1) {
+                break;
+            } else{
+                blarg.pop()
+            }
         }
-        console.log("Success!");
-        $ = cheerio.load(data);
-        var blarg = $("body").text();
-        // console.log(blarg);
-
-        tokenizer = new natural.WordTokenizer();
-        blarg = tokenizer.tokenize(blarg);
-        console.log(blarg.join("`"));
-
-
     });
 }
 
