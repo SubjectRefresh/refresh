48d9a042e2f5154f698f0061a4b48e4f84147beb
Pombearman <miles@budden.net>
2015-07-29 15:57:09 +0100

    Made searches easier

--- modules/convert.js
+++ modules/convert.js
@@ -1,9 +1,6 @@
 var colors = require("colors");
 console.log("Convert.JS:".bold + " Successfully Imported Required Packages".blue);
 
-
-var myStringArray = ["State the distinguishing properties of solids, liquids and gases", "State which parts of YRS are the best", "Describe the structure of solids, liquids and gases in terms of particle separation, arrangement and types of motion", "Describe changes of state in terms of melting, boiling, evaporation, freezing, condensation and sublimation", "Describe qualitatively the pressure and temperature of a gas in terms of the motion of its particles", "Show an understanding of the random motion of particles in a suspension (sometimes known as Brownian motion) as evidence for the kinetic particle (atoms, molecules or ions) model of matter", "Describe and explain diffusion"];
-
 var convertModule = function () {
     var self = this;
 
@@ -37,7 +34,7 @@ var convertModule = function () {
                 body: "apiKey=c0dbc052930dce78cc1dd1b37b3d3a4fb3f609c251c4f7e34a3b452a&text=" + utf8.encode(sentence) + "&extractors=" + utf8.encode("entities,dependency-trees")
             });
             var data = JSON.parse(res.getBody().toString('utf8'));
-            console.log(res.getBody().toString('utf8'));
+            //console.log(res.getBody().toString('utf8'));
             console.log("Test.JS:".bold + " Successfully attempted communication with TextRazor".green);
             var entities = data.response.entities;
             if (entities != undefined) {
@@ -50,7 +47,7 @@ var convertModule = function () {
                             for (j = 0; j < data.response.sentences[0].words.length; j++) {
                                 if (data.response.sentences[0].words[j].parentPosition == undefined) {
                                     console.log(data.response.sentences[0].words[j].token);
-                                    output.push("GCSE " + entities[g].entityEnglishId + " " + data.response.sentences[0].words[j].token);
+                                    output.push(data.response.sentences[0].words[j].token + " GCSE " + entities[g].entityEnglishId);
                                 }
                             }
 
