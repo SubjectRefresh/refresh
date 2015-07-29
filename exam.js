b869e115cead9a5313016fe87357324f53520c39
Oli Callaghan <OliCallaghan@icloud.com>
2015-07-29 11:39:59 +0100

    List.JS and Exam.JS

--- exam.js
+++ exam.js
@@ -3,49 +3,59 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var colors = require('colors');
 
-//console.log("Exam.JS:".bold + " Successfully Imported Required Packages".green);
-
 var examBoardModule = function() {
     var self = this;
+    
+    Array.prototype.last = function() {
+        return this[this.length - 1];
+    }
 
-    self.examBoardCIE = function(callback) {
-        request('http://www.cie.org.uk/programmes-and-qualifications/cambridge-secondary-2/cambridge-igcse/subjects/', function(error, response, body) {
+    self.collectURLs function(number, convert, callback) {
+        // Number Parameter Must be a String
+        var newArray = [];
+        var links = {
+            syllabus: null,
+            pdfs: []
+        };
+        console.log("PDFList.js: ".bold + "Successfully Defined Global Variables".green);
+        request("http://www.cie.org.uk/programmes-and-qualifications/cambridge-secondary-2/cambridge-igcse/subjects/", function(error, response, body) {
             if (!error && response.statusCode == 200) {
-                console.log("Exam.JS:".bold + " Successfully Requested Subject List".green);
+                console.log("PDFList.js: ".bold + "Successfully Requested Website For List of Subjects".green);
                 $ = cheerio.load(body);
-
-                data = cleanArray($('.emphasized-link').text().replace(/ +?|\r/g, '').split("\n"));
-                subjectArray = [];
-
-                for (i = 0; i < data.length; i++) {
-                    if (data[i] != "New" && data[i] != "Live") {
-                        subjectArray.push(data[i]);
+                $(".emphasized-link").find("li").each(function() { // loop through each link
+                    var selector = $(this).find("a").attr("href");
+                    newArray.push({
+                        "dom_object": $(this), // this is so we can access it later
+                        "number": selector.split("-").last().replace("/", ""), // number
+                        "link": baseURL + selector // link
+                    });
+                });
+                for (i = 0; i < newArray.length; i++) {
+                    if (String(newArray[i].number) == String(number)) { // we got a match for the subject
+                        links.syllabus = newArray[i].link;
+                        request(baseURL + newArray[i].dom_object.find("a").attr("href"), function(error2, response2, body2) {
+                            console.log("PDFList.js: ".bold + "Successfully Requested Website For List Of PDFs".green);
+                            $new = cheerio.load(body2);
+                            $new(".binaryLink").find("a").each(function() {
+                                var PDFLink = $new(this).attr("href");
+                                links.pdfs.push(PDFLink); // is working
+                                //    console.log("PDFList.js: ".bold + $new(this).text().blue + " => " + baseURL.green + PDFLink.green);
+                            });
+                            //console.log("PDFList.js: ".bold + "Got ".green + String(links.pdfs.length).blue + " PDFs".green);
+                        });
                     }
                 }
-
-                for (i = 0; i < subjectArray.length; i++) {
-                    subjectArray[i] = subjectArray[i].replace(/-/g, " - ");
+                console.log("collectURLs has finished".red);
+                if (convert) {
+                    doPDFConversions(links, function(html) { // insert pdf conversion function here and return HTML to the callback
+                        if (callback !== undefined) {
+                            callback(html);
+                        }
+                    });
                 }
-
-                subjectArray.length = subjectArray.length - 4;
-                //console.log(subjectArray);
-                console.log("Exam.JS:".bold + " Successfully Got `subjectArray`".green);
-                //callback(subjectArray); //Future Callback
             }
         });
     };
-
-    function cleanArray(actual) {
-        var newArray = new Array();
-        for (var i = 0; i < actual.length; i++) {
-            if (actual[i]) {
-                newArray.push(actual[i]);
-            }
-        }
-        return newArray;
-    }
-    //self.examBoardCIE(); //Temporary Use for Testing
-    //console.log("Exam.JS:".bold + " Is live".green);
 };
 
 module.exports = examBoardModule;
---
+++ list.js
@@ -0,0 +1,46 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var colors = require('colors');
+
+var examBoardModule = function() {
+    var self = this;
+
+    self.examBoardCIE = function(callback) {
+        request('http://www.cie.org.uk/programmes-and-qualifications/cambridge-secondary-2/cambridge-igcse/subjects/', function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("Exam.JS:".bold + " Successfully Requested Subject List".green);
+                $ = cheerio.load(body);
+
+                data = cleanArray($('.emphasized-link').text().replace(/ +?|\r/g, '').split("\n"));
+                subjectArray = [];
+
+                for (i = 0; i < data.length; i++) {
+                    if (data[i] != "New" && data[i] != "Live") {
+                        subjectArray.push(data[i]);
+                    }
+                }
+
+                for (i = 0; i < subjectArray.length; i++) {
+                    subjectArray[i] = subjectArray[i].replace(/-/g, " - ");
+                }
+
+                subjectArray.length = subjectArray.length - 4;
+                console.log("Exam.JS:".bold + " Successfully Got `subjectArray`".green);
+                //callback(subjectArray); //Future Callback
+            }
+        });
+    };
+
+    function cleanArray(actual) {
+        var newArray = new Array();
+        for (var i = 0; i < actual.length; i++) {
+            if (actual[i]) {
+                newArray.push(actual[i]);
+            }
+        }
+        return newArray;
+    }
+};
+
+module.exports = examBoardModule;
