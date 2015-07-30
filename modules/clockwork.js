43ea8d48c74fe45e60db6526c77421b557cc5dc4
Popey Gilbert <popey@debenclipper.com>
2015-07-30 10:58:11 +0100

    Clockwork Function

--- modules/clockwork.js
+++ modules/clockwork.js
@@ -1,11 +1,13 @@
 var clockwork = require("clockwork")({key:"073752104caacc80b8b874aa8c0deee8a7da4fe7"});
  
- 
+ function sendMessage(phone){
 // Send a message
-clockwork.sendSms({ To: "447858987130", Content: "Refresh - You haven't looked at your page in a while!  Come back and learn some more."}, function(error, resp) {
+clockwork.sendSms({ To: phone, Content: "Refresh - You haven't looked at your page in a while!  Come back and learn some more."}, function(error, resp) {
     if (error) {
         console.log("Something went wrong", error);
     } else {
         console.log("Message sent");
     }
-});
+});}
+
+sendMessage("447858987130");
