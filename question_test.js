1aa80e7b1260b83257c96d1670a62e197b74bf8f
Pombearman <miles@budden.net>
2015-07-31 20:45:49 +0100

    Much

--- question_test.js
+++ question_test.js
@@ -2,26 +2,37 @@ var utf8 = require('utf8');
 var request = require('sync-request');
 var colors = require("colors");
 
-function question(inputString, callback) {
+function question(inputArray, callback) {
 
-    var sentence = inputString;
-    //var sentence = inputString.join(" ");
-    var output = "";
-    var entityPositions = [];
-    var entityPositionsContentWrapper = [];
-    var res = request('POST', 'https://api.textrazor.com', {
-        body: "apiKey=c0dbc052930dce78cc1dd1b37b3d3a4fb3f609c251c4f7e34a3b452a&text=" + utf8.encode(inputString) + "&extractors=" + utf8.encode("entities")
-    });
-    console.log(res.getBody().toString('utf8'));
-    var data = JSON.parse(res.getBody().toString('utf8'));
-    for (i = 0; i < data.response.entities.length; i++) {
-        entityPositions.push([data.response.entities[i].matchedText, data.response.entities[i].startingPos, data.response.entities[i].endingPos]);
+    var output = [];
+    var outputFinal = [];
+    console.log("len: " + inputArray.length);
+    for (i = 0; i < inputArray.length; i++) {
+        console.log("LOOP!!!!!!!!");
+        var sentence = inputArray[i].join(" ");
+        var entityPositions = [];
+        var res = request('POST', 'https://api.textrazor.com', {
+            body: "apiKey=c0dbc052930dce78cc1dd1b37b3d3a4fb3f609c251c4f7e34a3b452a&text=" + utf8.encode(sentence) + "&extractors=" + utf8.encode("entities")
+        });
+        //console.log(res.getBody().toString('utf8'));
+        var data = JSON.parse(res.getBody().toString('utf8'));
+        for (i = 0; i < data.response.entities.length; i++) {
+            entityPositions.push([data.response.entities[i].matchedText, data.response.entities[i].startingPos, data.response.entities[i].endingPos]);
+        }
+        output = [entityPositions, sentence];
+        outputFinal.push(output);
+        console.log("output=========");
+        console.log(output);
+        console.log("^^^^^^^^^^^^");
+        //console.log(entityPositions);
     }
-    console.log(entityPositions);
-    callback(output);
+    callback(outputFinal);
 }
 
 
-question("Solid is one of the four fundamental states of matter ). It is characterized by structural rigidity and resistance to changes of shape or volume. Unlike a liquid , a solid object does not flow to take on the shape of its container , nor does it expand to fill the entire volume available to it like a gas does. The atoms in a solid are tightly bound to each other , either in a regular geometric lattice ) or irregularly )", function (output) {
-    console.log(output);
+question([[["This is a sentence containing some very iportant data", "This is a sentence about atoms.  Atoms are very mmagical things that make up all of the things in the universe."], ["This is a sentence containing some very iportant data", "This is a sentence about atoms.  Atoms are very mmagical things that make up all of the things in the universe."]], [["This is a sentence containing some very iportant data", "This is a sentence about atoms.  Atoms are very mmagical things that make up all of the things in the universe."], ["This is a sentence containing some very iportant data", "This is a sentence about atoms.  Atoms are very mmagical things that make up all of the things in the universe."]]], function (outputs) {
+            console.log(outputs);
 });
+
+
+[[[],"sdfjsdgfsf"],[]]
