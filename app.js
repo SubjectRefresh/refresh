9f998c1c2823f60e7f25cb131127bad827d72c01
Finnian Anderson <finnian@fxapi.co.uk>
2015-08-01 22:15:19 +0100

    Merge branch master/origin

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var bodyParser = require("body-parser");
 var multer = require("multer");
 var fs = require("fs");
 var colors = require("colors");
+var cookieParser = require('cookie-parser');
+var Cookies = require("cookies");
 
 colors.setTheme({
     title: ['white', 'italic'],
@@ -51,94 +53,134 @@ app.use(bodyParser.urlencoded({
 app.use(multer());
 console.log("App.JS: ".title + " Successfully Initialised Middleware".success);
 
-
 // Creating Static Public Folder
 app.use(express.static('public'));
 console.log("App.JS: ".title + " Successfully Created Public Folder".success);
 
-
 // App Routes
-app.get("/", function (req, res) {
-    fs.readFile("pages/index.html", "ASCII", function (err, data) {
+app.get("/", function(req, res) {
+    fs.readFile("pages/index.html", "ASCII", function(err, data) {
         res.send(data);
     });
 });
 
-app.post("/register", function (req, res) {
+app.post("/register", function(req, res) {
     var firstName = req.body.fName;
     var lastName = req.body.lName;
     var email = req.body.eMail;
     var password = req.body.pass;
     var username = req.body.uName;
-    
+
     databaseModule.addUser(firstName, lastName, email, password, username, function(output) {
         if (output == true) {
             fs.readFile("pages/syllabus-choice.html", "ASCII", function(err, data) {
                 res.send(data);
-            });  
-        }               
-        
+            });
+        }
+
     });
 });
 
-app.get("/finnTest", function (req, res) {
-    fs.readFile("pages/syllabus-choice.html", "ASCII", function (err, data) {
+app.get("/finnTest", function(req, res) {
+    fs.readFile("pages/syllabus-choice.html", "ASCII", function(err, data) {
         res.send(data);
     });
 });
 
-app.get("/learn", function (req, res) {
-    fs.readFile("pages/learn.html", "ASCII", function (err, data) {
+app.get("/learn", function(req, res) {
+    fs.readFile("pages/learn.html", "ASCII", function(err, data) {
         res.send(data);
     });
 });
 
-app.get("/dashboard", function (req, res) {
-    fs.readFile("pages/dashboard.html", "ASCII", function (err, data) {
+app.get("/dashboard", function(req, res) {
+    fs.readFile("pages/dashboard.html", "ASCII", function(err, data) {
         res.send(data);
     });
 });
 
-app.get("/login", function (req, res) {
-    fs.readFile("pages/login.html", "ASCII", function (err, data) {
+app.get("/login", function(req, res) {
+    fs.readFile("pages/login.html", "ASCII", function(err, data) {
         res.send(data);
     });
 });
 
-app.post("/CIE", function (req, res) {
-    listModule.examBoardCIE(function (data) {
+app.get("/logout", function(req, res) {
+    res.clearCookie('emai');
+    res.clearCookie('loggedin');
+    res.redirect("/", 301);
+});
+
+app.post("/checkLogin", function(req, res) {
+    var cookies = new Cookies(req, res),
+        unsigned, signed, tampered;
+    var password = req.body.password;
+    var email = req.body.email;
+
+    databaseModule.login(email, password, function(output) {
+        if (output != false) {
+            console.log(output.Email, output.FirstName, output.LastName);
+            cookies.set("emai", output.Email, {
+                httpOnly: false
+            });
+            res.cookie('emai', output.Email, {
+                maxAge: 900000,
+                httpOnly: true
+            });
+            cookies.set("forename", output.FirstName, {
+                httpOnly: false
+            });
+            cookies.set("surname", output.LastName, {
+                httpOnly: false
+            });
+            cookies.set("loggedin", true, {
+                httpOnly: false
+            });
+            res.redirect(301, '/dashboard?s=loggedin&email=' + email);
+        } else {
+            res.redirect(301, '/dashboard?s=incorrect_username_password');
+        }
+    });
+});
+
+
+app.post("/CIE", function(req, res) {
+    listModule.examBoardCIE(function(data) {
         res.send({
             subjectData: data
         });
     });
 });
 
-app.post("/CIEsubject", function (req, res) {
+
+app.post("/CIEsubject", function(req, res) {
     var syllabusNumber = String(req.body.syllabusNumber);
     //syllabusNumber = "0620";
     console.log(syllabusNumber);
-    examBoardModule.collectURLs(syllabusNumber, function (data) {
+    examBoardModule.collectURLs(syllabusNumber, function(data) {
         res.send(data);
     });
 });
 
-app.post("/dashboard", function (req, res) {
+app.post("/dashboard", function(req, res) {
     var email = req.body.email;
     var password = req.body.password;
     var examBoard = req.body.examBoard;
     var subject = req.body.subject;
     var syllabus = req.body.syllabus;
     var url = req.body.url;
-    databaseModule.login(email, password, function (output) {
+    databaseModule.login(email, password, function(output) {
         if (output == true) {
-            databaseModule.createSyllabusEntry(email, examBoard, subject, syllabus, function () {
-                scrapeModule.convertPDF(examBoard, subject, syllabus, url, function () {
-                    scrapeModule.scrape(examBoard, subject, syllabus, function (points) {
-                        convertModule.convert(points, function (searchFields) {
-                            researchModule.researchTopic(searchFields, function (usefulSentences) {
-                                questionModule.question(usefulSentences, function (toStore) {
-                                    fs.writeFile("files/" + "0620" + ".sentenceData", toStore, function (err) {
-                                        if (err) throw err;
+            databaseModule.createSyllabusEntry(email, examBoard, subject, syllabus, function() {
+                scrapeModule.convertPDF(examBoard, subject, syllabus, url, function() {
+                    scrapeModule.scrape(examBoard, subject, syllabus, function(points) {
+                        convertModule.convert(points, function(searchFields) {
+                            researchModule.researchTopic(searchFields, function(usefulSentences) {
+                                questionModule.question(usefulSentences, function(toStore) {
+                                    fs.writeFile("files/" + "0620" + ".sentenceData", toStore, function(err) {
+                                        if (err) {
+                                            console.log(err);
+                                        }
                                         res.send("HEY! :D");
                                     });
                                 });
@@ -154,13 +196,12 @@ app.post("/dashboard", function (req, res) {
 });
 
 // Initialising the Express.JS Web Server to Listen on Port process.argv[2]
-if (process.argv[2]){
-    
-}
-else {
+if (process.argv[2]) {
+
+} else {
     process.argv[2] = 80;
 }
-var server = app.listen(process.argv[2], function () {
+var server = app.listen(process.argv[2], function() {
     var host = server.address().address;
     var port = server.address().port;
 
